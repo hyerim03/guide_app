e5da734f5d3237a15fb081258eff9deb58824608
hyerim03 <chhari0708@naver.com>
2026-02-24 15:25:52 +0900

    fix: description page custom

--- screens/Description.jsx
+++ screens/Description.jsx
@@ -1,13 +1,35 @@
 import { Text, StatusBar, StyleSheet } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import useMqtt from '../hook/useMqttFKiosk';
+import { useState, useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 const Description = () => {
+  const [state, setState] = useState();
+  const [step, setStep] = useState(' ');
+  const { subscribe } = useMqtt();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    subscribe('ping', setState);
+
+    if (state?.includes('AMR')) {
+      setStep('AMR 공정');
+    }
+    if (state?.includes('MMR')) {
+      setStep('MMR 공정');
+    }
+    if (state == 'MMR_END') {
+      navigation.navigate('start');
+    }
+  }, [state]);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar hidden={true} />
       <LinearGradient style={styles.container} colors={['#CCDFFF', '#ffffff']}>
-        <Text style={styles.mainText}>시연중입니다...</Text>
+        <Text style={styles.mainText}>{step} 시연중입니다...</Text>
       </LinearGradient>
     </SafeAreaView>
   );
